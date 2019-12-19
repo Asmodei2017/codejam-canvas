f1f53018011818f32206fbf5fb4dcc6273a64bcd
Asmodei2017 <p.zabavskiy@tut.by>
2019-12-19 07:10:25 +0200

    feat: add image and json files

---
+++ data/4x4.js
@@ -0,0 +1,7 @@
+export const array_4x4 = 
+[
+    ["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
+    ["FFEB3B", "FFC107","FFC107","FFEB3B"],
+    ["FFEB3B", "FFC107","FFC107","FFEB3B"],
+    ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]
+];
